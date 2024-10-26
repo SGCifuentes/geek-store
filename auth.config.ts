9ba72836633a5a0efc14afd83480764acc1cc262
SGCifuentes <sebastiangc19.01@gmail.com>
2024-10-26 00:17:14 -0500

    add basic log out button

--- auth.config.ts
+++ auth.config.ts
@@ -8,15 +8,21 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
+
       const isOnAdmin = nextUrl.pathname.startsWith('/admin/dashboard');
       if (isOnAdmin) {
         if (isLoggedIn) return true;
 
         return false;
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/admin/auth', nextUrl));
+        return true;
       }
-      return true;
+      return false;
+    },
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith('/admin')) return `${baseUrl}/${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
     }
   }
 } satisfies NextAuthConfig;
